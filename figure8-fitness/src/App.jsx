e47ac1f29a16749ce57c3d26a80765b6edee26ef
Lorr-etta <laurettaamankwah@gmail.com>
2025-09-03 21:50:04 +0000

    Set up Tailwind CSS and Hello World test

---
+++ figure8-fitness/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <h1 className="text-5xl font-bold text-green-400">
+        Hello World
+      </h1>
+    </div>
+  );
+}
+
+export default App;
